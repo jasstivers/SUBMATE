f7fd9b4bfa6e3c7ad0050878aad682eaf1430557
Stamati Stamatiou <stachan66@gmail.com>
2025-01-21 19:45:23 +0900

    Created submarines model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_18_075439) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_21_104235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "submarines", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_submarines_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_075439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "submarines", "users"
 end
