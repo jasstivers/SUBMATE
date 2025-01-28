6e0320acf14813e7c8c332eac1ac1fa9249591c7
Jakub Ohkado <jakuboohkado@gmail.com>
2025-01-29 00:36:09 +0900

    Owner can delete submarine

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Submarines routes with nested bookings
-  resources :submarines, only: [:index, :show, :new, :create] do
+  resources :submarines, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create, :index], path: "bookings"  # Nested bookings with index action
   end
 
   resources :bookings, only: :index
-
 end
