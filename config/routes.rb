3b5086023a427efa24a1334b0a7deaa628556047
Jasmine Stivers <jasstivers@gmail.com>
2025-01-28 22:37:52 +0900

    Got new subs working

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Submarines routes with nested bookings
-  resources :submarines, only: [:index, :show] do
+  resources :submarines, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :create, :index], path: "bookings"  # Nested bookings with index action
   end
 
